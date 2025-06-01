6fb33ba10a2fa34bdec606e21018aa97bc2cca92
Fei Liang <njliangfei@gmail.com>
2025-06-01 13:07:45 +0800

    fix: exports

--- lib/scribble.dart
+++ lib/scribble.dart
@@ -1,8 +1,8 @@
 /// Scribble is a lightweight library for freehand drawing in Flutter
 library scribble;
 
-export 'package:scribble/src/domain/model/sketch/sketch.dart';
-export 'package:scribble/src/view/notifier/scribble_notifier.dart';
-export 'package:scribble/src/view/scribble.dart';
-export 'package:scribble/src/view/scribble_sketch.dart';
-export 'package:scribble/src/view/state/scribble.state.dart';
+export 'src/domain/model/sketch/sketch.dart';
+export 'src/view/notifier/scribble_notifier.dart';
+export 'src/view/scribble.dart';
+export 'src/view/scribble_sketch.dart';
+export 'src/view/state/scribble.state.dart';
